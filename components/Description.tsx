d1c36f8923635e8dc08088dea06cc1a455d9933b
BrenC11 <brendancleaves@me.com>
2026-01-26 17:32:42 +0000

    Remove genre line from main description

--- components/Description.tsx
+++ components/Description.tsx
@@ -16,11 +16,11 @@ export default function Description() {
           Film Description
         </p>
         <p className="mt-6 text-2xl leading-relaxed text-ice/80">
-          CRAK HACK is a sci-fi horror comedy. In a near-future dominated by VR
-          intimacy, a lonely man’s attempt to save his long-distance
-          relationship with a high-tech device spirals into absurd and
-          horrifying chaos when hackers seize control, forcing him to confront
-          unspeakable scenarios in a desperate bid to regain his life.
+          In a near-future dominated by VR intimacy, a lonely man’s attempt to
+          save his long-distance relationship with a high-tech device spirals
+          into absurd and horrifying chaos when hackers seize control, forcing
+          him to confront unspeakable scenarios in a desperate bid to regain his
+          life.
         </p>
       </motion.div>
     </section>
